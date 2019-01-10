b520a53027f49bdefd02248cb5fba07da66cf903
Samuel Wiggins <SamWiggz@users.noreply.github.com>
2019-01-10 13:27:39 -0800

    1

--- Lab2/swigg002_lorte007_lab02_part1/swigg002_lorte007_lab02_part1/main.c
+++ Lab2/swigg002_lorte007_lab02_part1/swigg002_lorte007_lab02_part1/main.c
@@ -1,18 +1,32 @@
-/*
- * swigg002_lorte007_lab02_part1.c
- *
- * Created: 1/10/2019 11:33:49 AM
- * Author : samwi
- */ 
+/*	Partner(s) Name & E-mail: Samuel Wiggins (swigg002@ucr.edu) Leo Ortega (lorte007@ucr.edu)
+ *	Lab Section: 021
+ *	Assignment: Lab #2  Exercise #1 
+ *	Exercise Description: [optional - include for your own benefit]
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
 
 #include <avr/io.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
+	DDRA = 0x00; PORTA = 0x00;
+	DDRB = 0x00; PORTB = 0x00; 
+	unsigned char tmpB = 0x00; 
+	unsigned char tmpA = 0x00;
+	while (1) 
     {
-    }
+		tmpA = PINA & 0x03;
+		if(tmpA == 0x01){
+			tmpB = (tmpB & 0xFE) | 0x01;
+		}
+		else{
+			tmpB = (tmpB & 0xFE);	
+		}
+		PORTB = tmpB;
+	}
+	return 0;
 }
 
